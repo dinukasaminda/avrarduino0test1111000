43a388e819bd2e5f5e81eaff9dc96bfba5308a30
dinukasaminda <dinukasaminda@gmail.com>
2020-06-16 13:15:19 +0530

    edited

--- uploader.sh
+++ uploader.sh
@@ -1,3 +1,3 @@
 echo "Running uploader script"
 
-/bin/bash /dev/vapps/local/bin/avrdude -C /dev/vapps/avrarduino0test1111000/avrdude.conf -v -patmega328p -carduino -PCOM1 -b57600 -D -Uflash:w:/dev/vapps/avrarduino0test1111000/serial-read-write/serial-read-write.ino.eightanaloginputs.hex:i 
+/bin/sh /dev/vapps/local/bin/avrdude -C /dev/vapps/avrarduino0test1111000/avrdude.conf -v -patmega328p -carduino -PCOM1 -b57600 -D -Uflash:w:/dev/vapps/avrarduino0test1111000/serial-read-write/serial-read-write.ino.eightanaloginputs.hex:i 
